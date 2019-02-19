90c2656064f228fc2f719e68bdbfc4502dbffc3b
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-02-19 00:02:45 -0600

    Created Class/Header files
    Created header files to go with each class file. Implemented header files and class files into the main file. Created a few weapons.

--- Monster.cpp
+++ Monster.cpp
@@ -1,48 +1,40 @@
 // Monster.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 #include <random>
+#include "Monster.h"
 using namespace std;
 
-class Monster {
-	public:
-		int hitpoints = hitpointSet();
-		int attackPower = attackPowerSet();
+	void Monster::hitpointCheck()
+	{
+		cout << hitpoints << " remaining." << endl;
+	}
 
-		void hitpointCheck()
+	void Monster::description() {
+		if (attackPower == 1) {
+			cout << "a hairy, mean looking ape." << endl;
+		}
+		else
 		{
-			cout << hitpoints << " remaining." << endl;
+			cout << "a large ape with red glowing eyes." << endl;
 		}
+		 
+	}
 
-		void description() {
-			if (attackPower == 1) {
-				cout << "a hairy, mean looking ape." << endl;
-			}
-			else
-			{
-				cout << "a large ape with red glowing eyes." << endl;
-			}
-			 
-		}
-	private:
-			int hitpointSet() {
-				// Randomly generataes a number from 1 to 3
-				random_device rd;
-				mt19937 gen(rd());
-				uniform_int_distribution<> dis(1, 3);
-				return dis(gen);
-			}
+	int Monster::hitpointSet() {
+		// Randomly generataes a number from 1 to 3
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_int_distribution<> dis(1, 3);
+		return dis(gen);
+	}
 
-			int attackPowerSet() {
-				// Randomly generataes a number from 1 to 2
-				random_device rd;
-				mt19937 gen(rd());
-				uniform_int_distribution<> dis(1, 2);
-				return dis(gen);
-			}
-
-		
-};
+	int Monster::attackPowerSet() {
+		// Randomly generataes a number from 1 to 2
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_int_distribution<> dis(1, 2);
+		return dis(gen);
+	}
 
--- dialog.cpp
+++ dialog.cpp
@@ -1,25 +1,14 @@
 //presumably we'll change this header
-#include "pch.h"
+
 #include <iostream>
 #include <string>
-#include "room.h"
-
+#include "dialog.h"
 using namespace std;
-
-class dialog
-{
-	room thisRoom;
-	string successfulOutcome;
-	string unsuccessfulOutcome;
-	string deathMessage;
-	string victoryMessage;
-	string greeting;
-	string warning;
 
 
 //constructors, can add more obv
-public:
-	dialog(const ::room& room, const string& successful_outcome, const string& unsuccessful_outcome,
+
+	dialog::dialog(const ::room& room, const string& successful_outcome, const string& unsuccessful_outcome,
 		const string& death_message, const string& victory_message, const string& greeting, const string& warning)
 		: room(room),
 		  successfulOutcome(successful_outcome),
@@ -33,73 +22,68 @@ public:
 
 	//getters and setters
 
-	string room_dialog() const
+	string dialog::room_dialog() const
 	{
 		return "You are in the " + room.name;
 	}
 
-	string get_successful_outcome() const
+	string dialog::get_successful_outcome() const
 	{
 		return successfulOutcome;
 	}
 
-	void set_successful_outcome(const string& successful_outcome)
+	void dialog::set_successful_outcome(const string& successful_outcome)
 	{
 		successfulOutcome = successful_outcome;
 	}
 
-	string get_unsuccessful_outcome() const
+	string dialog::get_unsuccessful_outcome() const
 	{
 		return unsuccessfulOutcome;
 	}
 
-	void set_unsuccessful_outcome(const string& unsuccessful_outcome)
+	void dialog::set_unsuccessful_outcome(const string& unsuccessful_outcome)
 	{
 		unsuccessfulOutcome = unsuccessful_outcome;
 	}
 
-	string get_death_message() const
+	string dialog::get_death_message() const
 	{
 		return deathMessage;
 	}
 
-	void set_death_message(const string& death_message)
+	void dialog::set_death_message(const string& death_message)
 	{
 		deathMessage = death_message;
 	}
 
-	string get_victory_message() const
+	string dialog::get_victory_message() const
 	{
 		return victoryMessage;
 	}
 
-	void set_victory_message(const string& victory_message)
+	void dialog::set_victory_message(const string& victory_message)
 	{
 		victoryMessage = victory_message;
 	}
 
-	string get_greeting() const
+	string dialog::get_greeting() const
 	{
 		return greeting;
 	}
 
-	void set_greeting(const string& greeting)
+	void dialog::set_greeting(const string& greeting)
 	{
 		this->greeting = greeting;
 	}
 
-	string get_warning() const
+	string dialog::get_warning() const
 	{
 		return warning;
 	}
 
-	void set_warning(const string& warning)
+	void dialog::set_warning(const string& warning)
 	{
 		this->warning = warning;
 	}
-
-
-
-};
-
 
